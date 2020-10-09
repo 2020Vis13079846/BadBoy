53b906016710b82d93cdd667ca39c169a53e92cd
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-10-09 23:39:53 +0300

    Update main.c

--- 2/main.c
+++ 2/main.c
@@ -1,17 +1,10 @@
+#include <string.h>
 #include <stdio.h>
-#include <ctype.h>
 
-int main() {
-    char* start = "There are no";
-    char* d = start;
-    char* s = d;
-
-    while (s) {
-        char c = *s++;
-        if (ispunct(c) || isspace(c)) {
-            continue;
-        }
-        *d++ = c;
-    }
-    printf("%s\n", start);
+int main(void)
+{
+    char buffer[120];
+    fgets(buffer, sizeof buffer, stdin);
+    strcat("foo", buffer);
+    return 0;
 }
